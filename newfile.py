853122e5ed753c40ef614ff6d1c17d6793655bdf
Chayruben <118160665+Chayruben@users.noreply.github.com>
2022-11-28 07:37:57 +0800

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,8 @@
+kg=float(input("Enter Kilogram (kg):"))
+ 
+klg=kg*2.205
+
+if klg > 25:
+          print("Your bag weighs:",klg, "lbs, your bag exeeds the weight limit")
+else:
+	print("Your bag did not exceed the weight limit, enjoy your flight!")
